50cfc9c2d7e9227ad3870713fcd9957d65e1d95a
Rodolfo Villas Boas <rodolfovillasboas@gmail.com>
2020-01-24 11:40:52 -0300

    Created sub-routes for child application and component for empty routes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,31 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
 
 import { AppComponent } from './app.component';
+import { Example1Component } from './example1/example1.component';
+import { EmptyRouteComponent } from './empty-route/empty-route.component';
+
+const ROUTES_MICRO_APP: Routes = [
+  {
+    path: 'app1',
+    component: Example1Component
+  },
+  {
+    path: '**',
+    component: EmptyRouteComponent
+  }
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    Example1Component,
+    EmptyRouteComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(ROUTES_MICRO_APP)
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/empty-route/empty-route.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'brad-empty-route',
+  template: '',
+})
+export class EmptyRouteComponent {
+}
