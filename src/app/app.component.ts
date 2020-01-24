77f51fe9b814eb045e17efc8755729f7427b460f
Rodolfo Villas Boas <rodolfovillasboas@gmail.com>
2020-01-24 11:11:53 -0300

    Created 'main.single-spa', 'asset-url' and 'single-spa-props' files to export child applications, assets and data. Based on the framework website

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,7 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'child-root',
+  selector: 'child-app1',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
 })
---
+++ src/main.single-spa.ts
@@ -0,0 +1,25 @@
+import 'core-js/es7/reflect';
+
+import { enableProdMode, NgZone } from '@angular/core';
+import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
+import { AppModule } from './app/app.module';
+import { environment } from './environments/environment';
+import singleSpaAngular from 'single-spa-angular';
+import { singleSpaPropsSubject } from './single-spa/single-spa-props';
+
+if (environment.production) {
+  enableProdMode();
+}
+
+const lifecycles = singleSpaAngular({
+  bootstrapFunction: singleSpaProps => {
+    singleSpaPropsSubject.next(singleSpaProps);
+    return platformBrowserDynamic().bootstrapModule(AppModule);
+  },
+  template: '<child-app1 />',
+  NgZone
+});
+
+export const bootstrap = lifecycles.bootstrap;
+export const mount = lifecycles.mount;
+export const unmount = lifecycles.unmount;
