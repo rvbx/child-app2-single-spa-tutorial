9a9e75aa707147a91fbce5d556bef4cbd46f98e9
Rodolfo Villas Boas <rodolfovillasboas@gmail.com>
2020-03-01 04:12:28 -0300

    Finalizacao da POC

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,10 @@
-import { Component } from '@angular/core';
+import { Component, ViewEncapsulation } from '@angular/core';
 
 @Component({
   selector: 'child-app2',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
+  encapsulation: ViewEncapsulation.ShadowDom
 })
 export class AppComponent {
   title = 'app2';
